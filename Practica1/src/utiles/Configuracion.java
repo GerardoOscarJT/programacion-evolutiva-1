8660c21e79264a459cff79c747cad8d456ec7bb6
aldurbey <aldurbey@localhost>
2012-03-29 12:50:33 +0200

    Seleciones finales

---
+++ Practica1/src/utiles/Configuracion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utiles;
+
+/**
+ *
+ * @author gerardo
+ */
+public class Configuracion {
+    public static boolean debugMode() {
+        return false;
+    }
+}
