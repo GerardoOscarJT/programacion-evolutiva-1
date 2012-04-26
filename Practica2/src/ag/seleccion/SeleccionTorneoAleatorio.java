dddd368ab23a8f494716b931e8a728362f782254
aldurbey <aldurbey@localhost>
2012-04-26 09:51:48 +0200

    Interfaz

---
+++ Practica2/src/ag/seleccion/SeleccionTorneoAleatorio.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ag.seleccion;
+
+/**
+ *
+ * @author gerardo
+ */
+public class SeleccionTorneoAleatorio extends Seleccion {
+
+    
+    
+}
