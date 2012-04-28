2e6b56551d6b1e5e13e6fed66875f45aa5c620ec
aldurbey <aldurbey@localhost>
2012-04-28 13:45:41 +0200

    torneos completados

--- Practica2/src/ag/seleccion/SeleccionTorneoDeterminista.java
+++ Practica2/src/ag/seleccion/SeleccionTorneoDeterminista.java
@@ -14,9 +14,23 @@ import java.util.ArrayList;
  */
 public class SeleccionTorneoDeterminista extends Seleccion {
 
-    @Override
+    public static int numParticipantes=3;
+    
     public ArrayList<Cromosoma> selecciona(int n, ArrayList<Cromosoma> poblacion) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ArrayList<Cromosoma> resultado = new ArrayList<Cromosoma>(n);
+        Cromosoma mejor;
+        while (n>0){
+            int x=utiles.Aleatorio.getRandomInt(poblacion.size());
+            mejor=poblacion.get(x);
+            for (int i=1;i<numParticipantes;i++){
+                int y=utiles.Aleatorio.getRandomInt(poblacion.size());
+                if (poblacion.get(y).evaluacion()>mejor.evaluacion())
+                    mejor=poblacion.get(x);
+            }
+            resultado.add(mejor);
+            n--;
+        }
+        return resultado;
     }
 
 }
