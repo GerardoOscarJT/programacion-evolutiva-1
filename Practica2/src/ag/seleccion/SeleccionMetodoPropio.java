38aaac2d0884807e7eb04f92762d4cb767cbade4
gerardo <gerardo@gerardo-VPCEA2S1E>
2012-04-30 00:13:40 +0200

    Selecci?n M?todo propio

--- Practica2/src/ag/seleccion/SeleccionMetodoPropio.java
+++ Practica2/src/ag/seleccion/SeleccionMetodoPropio.java
@@ -14,9 +14,45 @@ import java.util.ArrayList;
  */
 public class SeleccionMetodoPropio extends Seleccion {
 
-    @Override
+    int z = 5; // Número de comparaciones
+
+    /**
+     * La idea de este método de selección es seleccionar N cromosomas aleatorios
+     * y encontrar la pareja cuya evaluación sea lo más parecida posible.
+     * Este método de selección es válido para cualquier cromosoma.
+     */
     public ArrayList<Cromosoma> selecciona(int n, ArrayList<Cromosoma> poblacion) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        ArrayList<Cromosoma> resultado = new ArrayList<Cromosoma>(n);
+
+        for (;n>0;n--) {
+            int azar = utiles.Aleatorio.getRandomInt(poblacion.size());
+            Cromosoma elegido = poblacion.get(azar);
+            double evaluacion = elegido.evaluacion();
+
+
+            double diferencia = 99999999;
+            Cromosoma mejor = null;
+
+            for (int i=0; i<z; i++) {
+                Cromosoma tentativa = poblacion.get(utiles.Aleatorio.getRandomInt(poblacion.size()));
+                double tentativa_evaluacion = tentativa.evaluacion();
+
+                if (diferencia > Math.abs(evaluacion-tentativa_evaluacion)) {
+                    diferencia = Math.abs(evaluacion-tentativa_evaluacion);
+                    mejor = tentativa;
+                }
+
+            }
+
+            resultado.add(elegido);
+            resultado.add(mejor);
+
+            n--;
+
+        }
+
+        return poblacion;
     }
 
 }
